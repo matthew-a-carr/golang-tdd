7f8c1088a37775c363a7cd5d08aaab86682ce7d3
Matthew Carr <matt.carr@madetech.com>
2021-04-06 19:43:42 +0100

    Remove unused test cases

--- 05.structs/structs_test.go
+++ 05.structs/structs_test.go
@@ -19,7 +19,7 @@ func TestArea(t *testing.T) {
 	cases := []struct {
 		shape Shape
 		want  float64
-	}{{Rectange{10.0, 10.0}, 100.0}, {Circle{10.0}, 314.1592653589793}}
+	}{{shape: Rectange{10.0, 10.0}, want: 100.0}, {shape: Circle{10.0}, want: 314.1592653589793}}
 
 	for _, testCase := range cases {
 		shape := testCase.shape
@@ -30,25 +30,4 @@ func TestArea(t *testing.T) {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	}
-
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectange := Rectange{10.0, 10.0}
-		want := 100.0
-		checkArea(t, rectange, want)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
 }

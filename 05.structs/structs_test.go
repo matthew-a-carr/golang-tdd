49e29a12572f5231721f940738385567bbc4c6a0
Matthew Carr <matt.carr@madetech.com>
2021-04-06 19:47:00 +0100

    Improvement to test names

--- 05.structs/structs_test.go
+++ 05.structs/structs_test.go
@@ -17,17 +17,23 @@ func TestPerimiter(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	cases := []struct {
+		name  string
 		shape Shape
 		want  float64
-	}{{shape: Rectange{10.0, 10.0}, want: 100.0}, {shape: Circle{10.0}, want: 314.1592653589793}}
+	}{
+		{name: "Rectangle", shape: Rectange{10.0, 10.0}, want: 100.0},
+		{name: "Circle", shape: Circle{10.0}, want: 314.1592653589793},
+	}
 
 	for _, testCase := range cases {
-		shape := testCase.shape
-		got := shape.Area()
-		want := testCase.want
+		t.Run(testCase.name, func(t *testing.T) {
+			shape := testCase.shape
+			got := shape.Area()
+			want := testCase.want
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+			if got != want {
+				t.Errorf("%#v got %.2f want %.2f", shape, got, want)
+			}
+		})
 	}
 }
